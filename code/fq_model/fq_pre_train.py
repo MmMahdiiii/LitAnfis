a7e536d48f9206ce07e8564ceb75029535d701b1
MohammadMahdi <parchamimahdimohammad@gmail.com>
2024-04-30 14:22:15 +0330

    pretrain phase worked successfully

--- code/fq_model/fq_pre_train.py
+++ code/fq_model/fq_pre_train.py
@@ -6,24 +6,36 @@ from torch.nn.parameter import Parameter
 import torch.nn.functional as F
 
 
-class FQ_regression(nn.Module):
-    def __init__(self, in_features: int, rules: int, out_features: int, device=None, dtype=None):
+class FQ(nn.Module):
+    def __init__(self, in_features: int, rules: int, out_features: int, task='classification', fuzzy_model='mamdani', device=None, dtype=None):
         super().__init__()
         factory_kwargs = {'device': device, 'dtype': dtype}
         self.mean = nn.Parameter(torch.rand((1, in_features, rules), **factory_kwargs))
         self.std = nn.Parameter(torch.rand((1, in_features, rules), **factory_kwargs))
         self.tt = nn.Parameter(torch.randn((1, in_features, rules), **factory_kwargs) * 0.1)
-        self.decoder_linear = nn.Linear(in_features=rules * (in_features + 1), out_features=in_features)  # Decoder
-        self.output_linear = nn.Linear(in_features=rules * (in_features + 1), out_features=out_features)
+        self.decoder_linear = nn.Linear(in_features=rules, out_features=in_features)  # Decoder
+        if fuzzy_model == 'tsk':
+            self.output_linear = nn.Linear(in_features=rules * (in_features + 1), out_features=out_features)
+        elif fuzzy_model == 'mamdani':
+            self.output_linear = nn.Linear(in_features=rules, out_features=out_features)
+            
+        self.task = task
+        self.fuzzy_model = fuzzy_model
 
-    def forward(self, X, phase='pretrain'):
+    def forward(self, X, phase='final'):
         y = self.encode(X)
         if phase == 'pretrain':
             reconstructed_X = self.decoder_linear(y)
             return reconstructed_X
-        else:
-            y = self.consequent(y)
-            return y
+        elif self.fuzzy_model == 'tsk':
+            y = self.tsk(X, y)
+        elif self.fuzzy_model == 'mamdani':
+            y = self.mamdani(y)
+
+        if self.task == 'classification':
+            y = torch.softmax(y, dim=1)
+        
+        return y
 
     def encode(self, X):
         y = X.unsqueeze(2) - self.mean
@@ -36,9 +48,13 @@ class FQ_regression(nn.Module):
         y = y / f_sum
         return y
     
-    def consequent(self, X):
-        y = X.unsqueeze(1)
-        X = torch.cat([X, torch.ones((X.shape[0], 1)).to(y.device)], dim=1)
+    def mamdani(self, y):
+        y = self.output_linear(y)
+        return y
+    
+    def tsk(self, X, y):
+        y = y.unsqueeze(1)
+        X = torch.concatenate([X, torch.ones((X.shape[0], 1)).to(y.device)], dim=1)
         X = X.unsqueeze(2)
         y = X * y
         y = torch.flatten(y, start_dim=1)
